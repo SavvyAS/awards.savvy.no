3e586dce012d35dd00557a76e1cef497d1ae03d1
Erlend Rekve <erlend@savvy.no>
2023-01-02 14:47:41 +0100

    Added scoreboard and fixed styling

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,60 +1,47 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 import styles from './Nav.module.scss'
-import { useState } from 'react'
-import { MobileMenuModal } from '../Modals/MobileMenuModal'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEquals } from '@fortawesome/pro-regular-svg-icons'
+import {useState} from 'react'
 
 export const Nav = () => {
-  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false)
+    const [modalIsOpen, setModalIsOpen] = useState<boolean>(false)
 
-  const router = useRouter()
+    const router = useRouter()
 
-  const nav = [
-    { name: 'savvy.no', slug: '/clients' },
-  ]
+    const nav = [
+        {name: 'savvy.no', slug: '/clients'},
+    ]
 
-  return (
-    <>
-      <MobileMenuModal isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)} />
-      <nav className={styles.navbar}>
-        <div className="container">
-          <div className="column">
-            <div className={styles.navbar__inner}>
-              <div className={styles.navbar__logo}>
-                <Link href="/">
-                  <div>
-                    <img
-                      className="logo"
-                      src="/images/savvy-logo.svg"
-                      alt="Savvy logo"
-                      width="105"
-                      height="35"
-                    />
-                  </div>
-                </Link>
-              </div>
-              <div className={styles.navbar__links}>
-                <div className={styles['link-wrapper']}>
-                  <a href="https://savvy.no/" target="_blank" >
-                    <span>savvy.no</span>
-                  </a>
+    return (
+        <>
+            <nav className={styles.navbar}>
+                <div className="container">
+                    <div className="column">
+                        <div className={styles.navbar__inner}>
+                            <div className={styles.navbar__logo}>
+                                <Link href="/">
+                                    <div>
+                                        <img
+                                            className="logo"
+                                            src="/images/savvy-logo.svg"
+                                            alt="Savvy logo"
+                                            width="105"
+                                            height="35"
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className={styles.navbar__links}>
+                                <div className={styles['link-wrapper']}>
+                                    <a href="https://savvy.no/" target="_blank">
+                                        <span>savvy.no</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <button
-                className={styles['navbar__mobile-button']}
-                aria-label="Open menu"
-                onClick={() => setModalIsOpen(true)}
-              >
-                <FontAwesomeIcon icon={faEquals} />
-              </button>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </>
-  )
+            </nav>
+        </>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import content from '@/lib/content.json'
 import styles from './index.module.scss';
-import {GetStaticProps} from 'next/types'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faLongArrowRight} from "@fortawesome/pro-regular-svg-icons";
 import Image from 'next/image'
@@ -12,26 +11,34 @@ export default function Index({title}: Props) {
     const {home} = content.pages as any
     return (
         <main className={styles.container}>
-            <h1>{home.heading}</h1>
-            <p>{home.ingress}</p>
-            <a href="mailto:hakon@savvy.no" className={styles["send-contribution"]}>
-                {home.registerContribution}
-                <FontAwesomeIcon icon={faLongArrowRight}/>
-            </a>
-            <ul className={styles.points}>
-                {home.contributions.map((c: any) =>
-                    <li key={c.title}>
-                        <span>{c.points}</span>
-                        <span>{c.title}</span>
-                        <span>{c.description}</span>
-                    </li>
-                )}
-            </ul>
-            <h3>{home.prices.title}</h3>
-            <p>{home.prices.description}</p>
-            <div className={styles.prices}>
-                <ul>
-                    <li key={home.prices.yearEnd.title} className={styles.item}>
+            <section>
+
+                <h1>{home.heading}</h1>
+                <p>{home.ingress}</p>
+                <a href="mailto:hakon@savvy.no">
+                    {home.registerContribution}
+                    <FontAwesomeIcon icon={faLongArrowRight}/>
+                </a>
+            </section>
+            <section>
+
+                <ul className={styles.points}>
+                    {home.contributions.map((c: any) =>
+                        <li key={c.title}>
+                            <span>{c.points}</span>
+                            <span>{c.title}</span>
+                            <span>{c.description}</span>
+                        </li>
+                    )}
+                </ul>
+            </section>
+
+            <section>
+                <h2>{home.prices.title}</h2>
+                <p>{home.prices.description}</p>
+                <div className={styles.prices}>
+
+                    <div>
                         <Image
                             src={home.prices.yearEnd.imagePath}
                             quality="100"
@@ -40,68 +47,104 @@ export default function Index({title}: Props) {
                             alt="Bilde av Big Ben"
                         />
                         <span>
-                            {home.prices.yearEnd.title}
+                        {home.prices.yearEnd.title}
                             <span className={styles.purple}>{home.prices.yearEnd.description}</span>
-                        </span>
-                    </li>
-
-                    {home.prices.quarterly.map((x: PriceItem) =>
-                        <li key={x.title} className={styles.item}>
-                            {x.imagePath ?
-                                <Image
-                                    src={x.imagePath}
-                                    quality="100"
-                                    width={100}
-                                    height={100}
-                                    alt="Bilde av Big Ben"
-                                /> :
-                                <span className={styles.placeholder}>?</span>
-                            }
-                            <span>{x.title}
-                                <span className={styles.purple}>{x.description}</span>
+                    </span>
+                    </div>
+                    <ul>
+                        {home.prices.quarterly.map((x: PriceItem) =>
+                            <li key={x.title}>
+                                {x.imagePath ?
+                                    <Image
+                                        src={x.imagePath}
+                                        quality="100"
+                                        width={100}
+                                        height={100}
+                                        alt="Bilde av Big Ben"
+                                    /> :
+                                    <span className={styles.placeholder}>?</span>
+                                }
+                                <span>
+                                {x.title}
+                                    <span className={styles.purple}>{x.description}</span>
                             </span>
-                        </li>
-                    )}
-                </ul>
-            </div>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+
+            </section>
+
+            <section>
+
+                <h2 className={styles.purple}>{home.points.title}</h2>
+                <ScoreBoard data={home.points}/>
+            </section>
+
+            <section className={styles.footer}>
+                ©2021. All Rights Reserved
+            </section>
         </main>
     )
 }
 
-const ScoreBoard = (data: any) => {
+const ScoreBoard = ({data}: any) => {
 
-    const format = (items: any) => {
-        let persons: any = {};
-        items.forEach((x: any) => {
-            if (persons[x.contributor]) {
-                if (persons[x.contributor][x.category]) {
-                    persons[x.contributor][x.category] += 1;
-                } else {
-                    persons[x.contributor][x.category] = 1;
-                }
-                persons[x.contributor].totalScore += 1;
-            } else {
-                persons[x.contributor] = {};
-                persons[x.contributor][x.category] = 1;
-                persons[x.contributor].totalScore = 1;
+    const {contributors, contributions, categories} = data;
+
+    const format = (contributions: any) => {
+        let combined_contributions = contributions.reduce((current: any, contribution: any) => {
+            let contributor = current[contribution.contributor] ?? {};
+            contributor.totalScore = contributor.totalScore ? contributor.totalScore + 1 : 1;
+            contributor[contribution.category] = contributor[contribution.category] ? contributor[contribution.category] + 1 : 1;
+            return {...current, [contribution.contributor]: contributor}
+        }, {})
+
+        return Object.keys(combined_contributions).map(x => ({
+                id: x,
+                name: contributors[x].name,
+                image: contributors[x].image,
+                scores: {...combined_contributions[x]}
             }
-        });
-
+        )).sort((a, b) => b.scores.totalScore - a.scores.totalScore);
     }
 
     return (
-        <>
-            <h3>{data.title}</h3>
-
-
-        </>
-    );
-}
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-    return {
-        props: {
-            title: 'Awards'
-        }
-    }
+        <ul className={styles.scores}>
+            {format(contributions).map((x, i) =>
+                <li key={x.id}>
+                    <div className={styles["score-total"]}>
+                        <span>{i + 1}</span>
+                        <figure className={styles.image}>
+                            <Image
+                                src={x.image}
+                                quality="100"
+                                width={100}
+                                height={100}
+                                alt={`Bilde av ${x.name}`}
+                            />
+                        </figure>
+                        <span className={styles["content-block"]}>
+                            <span>{x.name}</span>
+                            <span>{`${x.scores.totalScore ?? "-"} ${data.totalScore}`}</span>
+                        </span>
+                    </div>
+                    <ul className={styles["score-details"]}>
+                        <li className={styles["content-block"]}>
+                            <span>{categories.internalPresentation}</span>
+                            <span>{x.scores.internalPresentation ?? "-"}</span>
+                        </li>
+                        <li className={styles["content-block"]}>
+                            <span>{categories.externalPresentation}</span>
+                            <span>{x.scores.externalPresentation ?? "-"}</span>
+                        </li>
+                        <li className={styles["content-block"]}>
+                            <span>{categories.article}</span>
+                            <span>{x.scores.article ?? "-"}</span>
+                        </li>
+                    </ul>
+                </li>
+            )}
+        </ul>
+    )
 }
